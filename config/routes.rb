fce0640daeb1959aaa73267e3cf655b21659b0e2
nmarini <nmarini44@gmail.com>
2019-03-27 11:09:41 -0700

    added .env file to root directory, updated gitignore to include .env file / refractored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'static_page/home'
   root 'static_page#home'
   get '/signup' => 'users#new'
   get '/login' => 'session#new'
   post '/login' => 'session#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   post '/logout' => 'session#destroy'
 
-  resources :tasks, only: [:index, :create, :destroy, :edit, :update]
-
-  resources :lists, only: [:index, :create, :destroy, :edit, :update] do
-    resources :tasks, only: [:new, :show]
+  resources :tasks, only: [:create, :destroy, :edit, :update] do 
+    resources :users, only: [:show]
   end 
 
-  resources :users, only: [:index, :create, :destroy, :edit, :update, :show] do
-    resources :lists, only: [:new, :show]
+  resources :lists, only: [:index, :show, :create, :destroy, :update] do
+    resources :tasks, only: [:new, :show, :edit]
+  end 
+
+  resources :users, only: [:create, :destroy, :edit, :update, :show] do
+    resources :lists, only: [:new, :edit, :show]
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
