524975346e197870994c02a0cef399ad8977f82f
nmarini <nmarini44@gmail.com>
2019-03-26 17:58:30 -0700

    added list controller actions and list_params private method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,32 @@
 class ListsController < ApplicationController
+
+    def index
+
+    end 
+
+    def new
+
+    end 
+
+    def create 
+
+    end 
+
+    def show
+
+    end 
+
+    def edit
+
+    end 
+
+    def update 
+
+    end 
+
+    private 
+
+    def list_params
+        params.require(:list).permit(:title, :user_id)
+    end 
 end
