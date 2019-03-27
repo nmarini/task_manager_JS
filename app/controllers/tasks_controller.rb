79e813974af0283825889d8d2765047d94e8aa86
nmarini <nmarini44@gmail.com>
2019-03-26 18:00:54 -0700

    added task controller actions and task_params private method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+
+    def new
+
+    end 
+
+    def create 
+
+    end 
+
+    def show
+
+    end 
+
+    def edit 
+
+    end 
+
+    def update
+
+    end
+
+    private 
+
+    def task_params
+        params.require(:task).permit(:title, :note, :list_id, :completed)
+    end 
 end
