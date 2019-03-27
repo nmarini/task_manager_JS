83e5542939fa01561fd2d240192aec0c1ce30d7f
nmarini <nmarini44@gmail.com>
2019-03-27 15:15:34 -0700

    updated nested routes so user can update a task, added link to task show view to edit if current_user

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
   
   resources :users do 
-    resources :tasks, only: [:show]
+    resources :tasks, only: [:show, :edit]
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
