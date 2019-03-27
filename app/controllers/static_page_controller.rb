b312f703948135d5e07dc0947bc4de4f23754f89
nmarini <nmarini44@gmail.com>
2019-03-26 17:56:03 -0700

    added skip_before_action to my controllers, and updated session controllre create and destroy actions

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,5 @@
 class StaticPageController < ApplicationController
+  skip_before_action :verify_user_is_authenticated, only: [:home]
   def home
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_user_is_authenticated, only: [:new,:create]
 
     def new 
         @user = User.new
