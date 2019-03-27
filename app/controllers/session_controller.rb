f94afd19aa195891a76d718dd82b24925c9e78fd
nmarini <nmarini44@gmail.com>
2019-03-27 12:27:34 -0700

    updated session#create to include facebook login, updated user.rb to include self.find_or_create_by_omniauth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,17 +4,36 @@ class SessionController < ApplicationController
   def new
   end
 
-  def create 
-    if @user = User.find_by(name: params[:user][:name])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      render 'new'
+  # @user = User.find_or_create_by(uid: auth['uid']) do |u|
+  #   u.name = auth['info']['name']
+  #   u.email = auth['info']['email']
+  #   u.image = auth['info']['image']
+  # end
+
+  # session[:user_id] = @user.id
+
+  def create
+    if auth_hash = request.env["omniauth.auth"]
+      user = User.find_or_create_by_omniauth(auth_hash)
+      session_login(user)
+    else 
+      user = User.find_by(email: params[:user][:email])
+      if user && user.authenticate(params[:user][:password])
+        session_login(user)
+      else
+        render controller: 'users', action: 'new'
     end 
   end 
 
   def destroy
     session.delete :user_id
+    redirect_to root_path
+  end 
+
+  private 
+
+  def session_login(user)
+    session[:user_id] = user.id
     redirect_to root_path
   end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     has_secure_password 
-    
+
     has_many :lists
     has_many :tasks, through: :lists
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        where(email: auth_hash["info"]["email"]).first_or_create do |user|
+            user.password = SecureRandom.hex 
+            user.name = auth_hash["info"]["name"]
+        end  
+    end 
 end
