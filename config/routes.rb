bc77b54f915e9fa13b3a8f827c914b226679f9b7
nmarini <nmarini44@gmail.com>
2019-03-27 14:23:16 -0700

    updated routes to include a task show route nested under users

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :destroy, :edit, :update, :show] do
     resources :lists, only: [:new, :edit, :show]
+  end
+  
+  resources :users do 
+    resources :tasks, only: [:show]
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
