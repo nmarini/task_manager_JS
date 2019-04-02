c7b012b41a701e3715d09319e9d3df6cbc9cf1fb
nmarini <nmarini44@gmail.com>
2019-04-02 14:05:24 -0700

    added a completed column to userstasks table

--- db/migrate/20190402014559_create_users_tasks.rb
+++ db/migrate/20190402014559_create_users_tasks.rb
@@ -3,6 +3,7 @@ class CreateUsersTasks < ActiveRecord::Migration[5.2]
     create_table :users_tasks do |t|
       t.integer :user_id
       t.integer :task_id
+      t.boolean :complete, default: false
       
       t.timestamps
     end
