b8c543a6ee520b48be2a321978b2e5bc9fd3039f
nmarini <nmarini44@gmail.com>
2019-04-22 15:08:14 -0700

    fixed spacing

--- app/models/users_task.rb
+++ app/models/users_task.rb
@@ -3,6 +3,6 @@ class UsersTask < ApplicationRecord
     belongs_to :task 
 
     def self.unaccepted_by_user(user)
-        UsersTask.where(accepted: false).select{|users_task| users_task.user_id == user.id}
+        UsersTask.where(accepted: false).select{ |users_task| users_task.user_id == user.id}
     end 
 end
