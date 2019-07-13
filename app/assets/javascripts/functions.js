0dc830d4d97481fd2dcd0e9a05e89bef3e9b2013
nmarini <nmarini44@gmail.com>
2019-07-13 16:29:19 -0700

    added functionality to task show button to show task's attributes and user array

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -8,53 +8,70 @@ function showListListener() {
     $('button#show-lists').on("click", function(event) {
         event.preventDefault();
         $.get('/lists.json', function(resp) {
-            resp.lists.forEach((li) => {
-                let id = li.id
-                let title = li.title;
-                let userId = li.user.id;
-                let list = new List(id, title, userId);
-                li.tasks.forEach((task) => {
-                    list.tasks.push({
-                        completed: task.completed
-                    })
-                })
-                let listItem = showList(list)
-                $("ol#lists").append(listItem);
-                $('button#show-lists').hide()
-                showTasksListener(list) 
+            showLists(resp.lists);
+        })
+    })
+}
+
+function showLists(lists) {
+    lists.forEach((li) => {
+        let id = li.id
+        let title = li.title;
+        let userId = li.user.id;
+        let list = new List(id, title, userId);
+        li.tasks.forEach((task) => {
+            list.tasks.push({
+                id: task.id,
+                title: task.title,
+                completed: task.completed
             })
         })
+        $("ol#lists").append(`<li id="data-list-${list.id}">${list.title} <br> <b>Status:</b> ${list.completed()} <button id="show-list-${list.id}">Show</button><br>
+        </li><br>`);
+        $('button#show-lists').hide()
+        listTasksListener(list) 
     })
 }
+    
 
-function showList(list) {
-    return `<li id="data-list-${list.id}">${list.title} <br> <b>Status:</b> ${list.completed()} <button id="show-list-${list.id}">Show</button><br>
-    </li><br>`
-}
-
-function showTasksListener(list) {
+function listTasksListener(list) {
     $(`button#show-list-${list.id}`).on("click", function(event) {
         event.preventDefault();
-        $.get(`/lists/` + list.id + `.json`, function(resp){
-            let li = resp.list
-            showTasks(li);            
-        })
-       
+        showListTasks(list);
+        $(`button#show-list-${list.id}`).hide()
     })
 }
 
-function showTasks(list) {
+function showListTasks(list) {
     $(`li#data-list-${list.id}`).append(`<ol id="list-${list.id}-tasks"></ol>`)
-        list.tasks.forEach((task) => {
-            $(`ol#list-${list.id}-tasks`).append(`<li id="list-${list.id}-task-${task.id}">${task.title} <br> <button id="show-task-${task.id}">Show</button></li><br>`)
-            taskItemListener(task);
-        })
+    list.tasks.forEach((task) => {
+        $(`ol#list-${list.id}-tasks`).append(`<li id="list-${list.id}-task-${task.id}">${task.title} <br> <button id="show-task-${task.id}">Show</button></li><br>`)
+        showTaskListener(list, task);
+    })
 }
 
-function taskItemListener(task) {
-
+function showTaskListener(list, task) { 
+    $(`button#show-task-${task.id}`).on("click", function(event) {
+        event.preventDefault();
+        showTask(list, task);
+        $(`button#show-task-${task.id}`).hide()
+    })
 }
 
-function taskItem(task) {
+function showTask(list, listTask) {
+    $.get("/lists/" + list.id + "/tasks/" + listTask.id + ".json", function(resp) {
+        let task = resp.task 
+        $(`li#list-${list.id}-task-${task.id}`).append(`<ol id="task-${task.id}"></ol>`)
+        $(`ol#task-${task.id}`).append(`<li id="task-${task.id}-info"> <b>Title:</b> ${task.title} <br>
+            <b>Complete:</b> ${task.completed} <br>
+            <b>Users Assigned:</b> <ol id="task-${task.id}-users"></ol> </li>`)
+        appendTaskUsers(task, task.users)
+    })
+}
 
+function appendTaskUsers(task, users) {
+    users.forEach((user) => {
+        $(`ol#task-${task.id}-users`).append(`<li>${user.name}</li>`)
+    })
+    
 }
