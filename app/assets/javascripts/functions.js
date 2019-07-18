21582e011b9b0329b33d9e7b93a3b36c4034300e
nmarini <nmarini44@gmail.com>
2019-07-18 13:13:02 -0700

    added task notes to showTask()

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -51,7 +51,7 @@ function showTask(list, listTask) {
     $.get("/lists/" + list.id + "/tasks/" + listTask.id + ".json", function(resp) {
         let task = resp.task 
         $(`li#list-${list.id}-task-${task.id}`).append(`<ul id="task-${task.id}"></ul>`)
-        $(`ul#task-${task.id}`).append(`<li id="task-${task.id}-info"> <b>Complete:</b> ${task.completed} <br>
+        $(`ul#task-${task.id}`).append(`<li id="task-${task.id}-info"> <b>Note:</b> ${task.note} <br> <b>Complete:</b> ${task.completed} <br>
             <b>Users Assigned:</b> <ol id="task-${task.id}-users"></ol> </li>`)
         appendTaskUsers(task, task.users)
     })
